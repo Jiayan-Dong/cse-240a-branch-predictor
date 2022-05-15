ed02195f51d5bdb5e7356c9db5cd11f341f1959f
Jiayan Dong <jid001@ucsd.edu>
2022-05-15 09:00:20 -0700

    python script for csv file results

---
+++ src/run_all.py
@@ -0,0 +1,36 @@
+import csv
+import os
+import sys
+import re
+
+"""
+usage python3 run_all.py bp_name
+"""
+
+traces = ['fp_1', 'fp_2', 'int_1', 'int_2', 'mm_1', 'mm_2']
+
+fileheader = ['benchmark', 'branches', 'incorrect', 'misprediction_rate']
+
+results = []
+
+overall_branches = 0
+overall_incorrect = 0
+
+for t in traces:
+    xe = os.popen(
+        "bunzip2 -kc ../traces/{}.bz2 | ./predictor --tournament".format(t), 'r')
+    output = xe.read()
+    result = re.findall(r"\d+\.?\d*", output)
+    results.append([t] + result)
+
+    overall_branches += int(result[0])
+    overall_incorrect += int(result[1])
+
+results = results + [['overall', str(overall_branches), str(
+    overall_incorrect), "{:.3f}".format(overall_incorrect * 100 / overall_branches)]]
+
+bp_name = sys.argv[1]
+
+with open('../results/{}.csv'.format(bp_name), 'w') as csvfile:
+    cwriter = csv.writer(csvfile)
+    cwriter.writerows([fileheader] + results)
