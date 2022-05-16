7f3daa71c3e3af446712fefe649072fcdc38baf4
Jiayan Dong <jid001@ucsd.edu>
2022-05-15 20:23:44 -0700

    cust = tour + bimode

--- src/predictor.c
+++ src/predictor.c
@@ -594,6 +594,338 @@ void cleanup_tage()
   }
 }
 
+int bimode_nt_ghistoryBits = 11;
+int bimode_t_ghistoryBits = 11;
+int ct_PCBits = 11;
+
+uint8_t *pht_nt_bimode;
+uint8_t *pht_t_bimode;
+uint8_t *ct_bimode;
+
+// bimode functions
+void init_bimode()
+{
+  int pht_nt_entries = 1 << bimode_nt_ghistoryBits;                    // 2^10 entries in LHT (Local histort table)
+  int pht_t_entries = 1 << bimode_t_ghistoryBits;                      // 2^11 entries in LPT (Local prediction table)
+  int choice_entries = 1 << ct_PCBits;                                 // 2^12 entries in CT (Choice prediction table)
+  pht_nt_bimode = (uint8_t *)malloc(pht_nt_entries * sizeof(uint8_t)); // 2^11 * 2 (bit counter) = 4 * 2^10 bits
+  pht_t_bimode = (uint8_t *)malloc(pht_t_entries * sizeof(uint8_t));   // 2^12 * 2 (bit counter) = 8 * 2^10 bits
+  ct_bimode = (uint8_t *)malloc(choice_entries * sizeof(uint8_t));     // 2^12 * 2 (bit counter) = 8 * 2^10 bits
+                                                                       // total: (11 + 4 + 8 + 8) * 2^10 bits = 31 * 2^10 bits = 31744 bits < 32Kbits = 32768 bits
+
+  memset(pht_nt_bimode, WN, pht_nt_entries);
+  memset(pht_t_bimode, WN, pht_t_entries);
+  memset(ct_bimode, WN, choice_entries);
+
+  ghistory = 0;
+}
+
+uint8_t bimode_predict(uint32_t pc)
+{
+  // get lower ghistoryBits of pc
+  uint32_t pht_nt_entries = 1 << bimode_nt_ghistoryBits;
+  uint32_t pc_lower_bits_nt = pc & (pht_nt_entries - 1);
+  uint32_t ghistory_lower_bits_nt = ghistory & (pht_nt_entries - 1);
+  uint32_t index_nt = pc_lower_bits_nt ^ ghistory_lower_bits_nt;
+
+  uint32_t pht_t_entries = 1 << bimode_t_ghistoryBits;
+  uint32_t pc_lower_bits_t = pc & (pht_t_entries - 1);
+  uint32_t ghistory_lower_bits_t = ghistory & (pht_t_entries - 1);
+  uint32_t index_t = pc_lower_bits_t ^ ghistory_lower_bits_t;
+
+  uint32_t ct_entries = 1 << ct_PCBits;
+  uint32_t ct_index = ghistory & (ct_entries - 1);
+
+  if (ct_bimode[ct_index] >= SN && ct_bimode[ct_index] <= WN)
+  {
+    switch (pht_nt_bimode[index_nt])
+    {
+    case WN:
+      return NOTTAKEN;
+    case SN:
+      return NOTTAKEN;
+    case WT:
+      return TAKEN;
+    case ST:
+      return TAKEN;
+    default:
+      printf("Warning: Undefined state of entry in Bimode PHT NT!\n");
+      return NOTTAKEN;
+    }
+  }
+  else if (ct_bimode[ct_index] >= WT && ct_bimode[ct_index] <= ST)
+  {
+    switch (pht_t_bimode[index_t])
+    {
+    case WN:
+      return NOTTAKEN;
+    case SN:
+      return NOTTAKEN;
+    case WT:
+      return TAKEN;
+    case ST:
+      return TAKEN;
+    default:
+      printf("Warning: Undefined state of entry in Bimode PHT T!\n");
+      return NOTTAKEN;
+    }
+  }
+  else
+  {
+    printf("Warning: Undefined state of entry in Bimode CT!\n");
+    return NOTTAKEN;
+  }
+}
+
+void train_bimode(uint32_t pc, uint8_t outcome)
+{
+  // get lower ghistoryBits of pc
+  uint32_t pht_nt_entries = 1 << bimode_nt_ghistoryBits;
+  uint32_t pc_lower_bits_nt = pc & (pht_nt_entries - 1);
+  uint32_t ghistory_lower_bits_nt = ghistory & (pht_nt_entries - 1);
+  uint32_t index_nt = pc_lower_bits_nt ^ ghistory_lower_bits_nt;
+
+  uint32_t pht_t_entries = 1 << bimode_t_ghistoryBits;
+  uint32_t pc_lower_bits_t = pc & (pht_t_entries - 1);
+  uint32_t ghistory_lower_bits_t = ghistory & (pht_t_entries - 1);
+  uint32_t index_t = pc_lower_bits_t ^ ghistory_lower_bits_t;
+
+  uint32_t ct_entries = 1 << ct_PCBits;
+  uint32_t ct_index = ghistory & (ct_entries - 1);
+
+  uint8_t wrong = 0;
+
+  if (ct_bimode[ct_index] >= SN && ct_bimode[ct_index] <= WN)
+  {
+    // Update state of entry in bht based on outcome
+    switch (pht_nt_bimode[index_nt])
+    {
+    case WN:
+      pht_nt_bimode[index_nt] = (outcome == TAKEN) ? WT : SN;
+      wrong = 1;
+      break;
+    case SN:
+      pht_nt_bimode[index_nt] = (outcome == TAKEN) ? WN : SN;
+      wrong = 1;
+      break;
+    case WT:
+      pht_nt_bimode[index_nt] = (outcome == TAKEN) ? ST : WN;
+      break;
+    case ST:
+      pht_nt_bimode[index_nt] = (outcome == TAKEN) ? ST : WT;
+      break;
+    default:
+      printf("Warning: Undefined state of entry in GSHARE BHT!\n");
+    }
+  }
+  else if (ct_bimode[ct_index] >= WT && ct_bimode[ct_index] <= ST)
+  {
+    // Update state of entry in bht based on outcome
+    switch (pht_t_bimode[index_t])
+    {
+    case WN:
+      pht_t_bimode[index_t] = (outcome == TAKEN) ? WT : SN;
+      wrong = 1;
+      break;
+    case SN:
+      pht_t_bimode[index_t] = (outcome == TAKEN) ? WN : SN;
+      wrong = 1;
+      break;
+    case WT:
+      pht_t_bimode[index_t] = (outcome == TAKEN) ? ST : WN;
+      break;
+    case ST:
+      pht_t_bimode[index_t] = (outcome == TAKEN) ? ST : WT;
+      break;
+    default:
+      printf("Warning: Undefined state of entry in GSHARE BHT!\n");
+    }
+  }
+  else
+  {
+    printf("Warning: Undefined state of entry in Bimode CT!\n");
+  }
+  if (wrong == 1 | ct_bimode[ct_index] == outcome)
+  {
+    switch (ct_bimode[ct_index])
+    {
+    case WN:
+      ct_bimode[ct_index] = (outcome == TAKEN) ? WT : SN;
+      break;
+    case SN:
+      ct_bimode[ct_index] = (outcome == TAKEN) ? WN : SN;
+      break;
+    case WT:
+      ct_bimode[ct_index] = (outcome == TAKEN) ? ST : WN;
+      break;
+    case ST:
+      ct_bimode[ct_index] = (outcome == TAKEN) ? ST : WT;
+      break;
+    default:
+      printf("Warning: Undefined state of entry in Bimode CT!\n");
+    }
+  }
+  ghistory = ((ghistory << 1) | outcome);
+}
+
+void cleanup_bimode()
+{
+  free(pht_nt_bimode);
+  free(pht_t_bimode);
+  free(ct_bimode);
+}
+
+// Cust
+uint64_t *lht_cust;
+uint8_t *lpt_cust;
+
+uint8_t *gpt_cust;
+
+uint8_t *ct_cust;
+
+int custLhistoryBits = 10; // Number of bits of saturating counter used for Local prediction
+int custLIndexBits = 10;   // Number of Program counter bits used for Local history table
+int custChoiceBits = 12;   // Number of Path history bits used for Choice prediction
+
+// cust functions
+void init_cust()
+{
+  int lht_entries = 1 << custLIndexBits;                         // 2^10 entries in LHT (Local histort table)
+  int lpt_entries = 1 << custLhistoryBits;                       // 2^10 entries in LPT (Local prediction table)
+  int choice_entries = 1 << custChoiceBits;                      // 2^12 entries in CT (Choice prediction table)
+  lht_cust = (uint64_t *)calloc(lht_entries, sizeof(uint64_t));  // 2^10 * 10 (bit counter) = 10 * 2^10 bits
+  lpt_cust = (uint8_t *)malloc(lpt_entries * sizeof(uint8_t));   // 2^10 * 2 (bit counter) = 2 * 2^10 bits
+  ct_cust = (uint8_t *)malloc(choice_entries * sizeof(uint8_t)); // 2^12 * 2 (bit counter) = 8 * 2^10 bits
+
+  memset(lpt_cust, WN, lht_entries);
+  memset(ct_cust, WN, choice_entries);
+
+  bimode_nt_ghistoryBits = 11;
+  bimode_t_ghistoryBits = 11;
+  ct_PCBits = 11;
+
+  init_bimode(); // 3 tables, 3 * 2^11 * 2 = 12 * 2^10
+                 // total: (10 + 2 + 8 + 12) * 2^10 = 2^15 = 32Kbits, + max global history register = 11 bits
+                 // budgets: 32Kbits + 11 bits
+  ghistory = 0;
+}
+
+uint8_t cust_predict(uint32_t pc)
+{
+  // get lower ghistoryBits of pc
+  uint32_t lht_entries = 1 << custLIndexBits;
+  uint32_t lht_index = pc & (lht_entries - 1);
+
+  uint32_t lpt_entries = 1 << custLhistoryBits;
+  uint32_t lpt_index = lht_cust[lht_index] & (lpt_entries - 1);
+
+  uint32_t ct_entries = 1 << custChoiceBits;
+  uint32_t ct_index = ghistory & (ct_entries - 1);
+
+  if (ct_cust[ct_index] >= SN && ct_cust[ct_index] <= WN)
+  {
+    switch (lpt_cust[lpt_index])
+    {
+    case WN:
+      return NOTTAKEN;
+    case SN:
+      return NOTTAKEN;
+    case WT:
+      return TAKEN;
+    case ST:
+      return TAKEN;
+    default:
+      printf("Warning: Undefined state of entry in cust LPT!\n");
+      return NOTTAKEN;
+    }
+  }
+  else if (ct_cust[ct_index] >= WT && ct_cust[ct_index] <= ST)
+  {
+    return bimode_predict(pc);
+  }
+  else
+  {
+    printf("Warning: Undefined state of entry in cust CT!\n");
+    return NOTTAKEN;
+  }
+}
+
+void train_cust(uint32_t pc, uint8_t outcome)
+{
+  // get lower ghistoryBits of pc
+  uint32_t lht_entries = 1 << custLIndexBits;
+  uint32_t lht_index = pc & (lht_entries - 1);
+
+  uint32_t lpt_entries = 1 << custLhistoryBits;
+  uint32_t lpt_index = lht_cust[lht_index] & (lpt_entries - 1);
+
+  uint32_t ct_entries = 1 << custChoiceBits;
+  uint32_t ct_index = ghistory & (ct_entries - 1);
+
+  uint8_t lpt_outcome;
+  uint8_t bimode_outcome = bimode_predict(pc);
+
+  // Update state of entry in bht based on outcome
+  switch (lpt_cust[lpt_index])
+  {
+  case WN:
+    lpt_cust[lpt_index] = (outcome == TAKEN) ? WT : SN;
+    lpt_outcome = NOTTAKEN;
+    break;
+  case SN:
+    lpt_cust[lpt_index] = (outcome == TAKEN) ? WN : SN;
+    lpt_outcome = NOTTAKEN;
+    break;
+  case WT:
+    lpt_cust[lpt_index] = (outcome == TAKEN) ? ST : WN;
+    lpt_outcome = TAKEN;
+    break;
+  case ST:
+    lpt_cust[lpt_index] = (outcome == TAKEN) ? ST : WT;
+    lpt_outcome = TAKEN;
+    break;
+  default:
+    printf("Warning: Undefined state of entry in GSHARE BHT!\n");
+  }
+
+  train_bimode(pc, outcome);
+  ghistory = (ghistory >> 1);
+
+  if ((lpt_cust[lpt_index] == ST || lpt_cust[lpt_index] == WT) ^ (bimode_outcome == TAKEN))
+  {
+    switch (ct_cust[ct_index])
+    {
+    case WN:
+      ct_cust[ct_index] = (outcome == lpt_outcome) ? SN : WT;
+      break;
+    case SN:
+      ct_cust[ct_index] = (outcome == lpt_outcome) ? SN : WN;
+      break;
+    case WT:
+      ct_cust[ct_index] = (outcome == bimode_outcome) ? ST : WN;
+      break;
+    case ST:
+      ct_cust[ct_index] = (outcome == bimode_outcome) ? ST : WT;
+      break;
+    default:
+      printf("Warning: Undefined state of entry in GSHARE BHT!\n");
+    }
+  }
+
+  // Update history register
+  lht_cust[lht_index] = ((lht_cust[lht_index] << 1) | outcome);
+  ghistory = ((ghistory << 1) | outcome);
+}
+
+void cleanup_cust()
+{
+  free(lht_cust);
+  free(lpt_cust);
+  free(gpt_cust);
+  free(ct_cust);
+  cleanup_bimode();
+}
+
 void init_predictor()
 {
   switch (bpType)
@@ -606,7 +938,9 @@ void init_predictor()
     init_alpha21264();
     break;
   case CUSTOM:
-    init_tage();
+    // init_tage();
+    // init_bimode();
+    init_cust();
   default:
     break;
   }
@@ -629,7 +963,9 @@ uint8_t make_prediction(uint32_t pc)
   case TOURNAMENT:
     return alpha21264_predict(pc);
   case CUSTOM:
-    return tage_predict(pc);
+    // return tage_predict(pc);
+    // return bimode_predict(pc);
+    return cust_predict(pc);
   default:
     break;
   }
@@ -654,7 +990,9 @@ void train_predictor(uint32_t pc, uint8_t outcome)
   case TOURNAMENT:
     return train_alpha21264(pc, outcome);
   case CUSTOM:
-    return train_tage(pc, outcome);
+    // return train_tage(pc, outcome);
+    // return train_bimode(pc, outcome);
+    train_cust(pc, outcome);
   default:
     break;
   }
